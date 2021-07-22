a2762186babe87c9d21b736e00acc47c4eb220f3
amitabhajayabiswal <87791489+amitabhajayabiswal@users.noreply.github.com>
2021-07-22 09:50:47 +0530

    Removing comments

--- huffmann-coding/src/main/java/com/perennialsys/App.java
+++ huffmann-coding/src/main/java/com/perennialsys/App.java
@@ -7,7 +7,6 @@ public class App
 {
     public static void main( String[] args )
     {
-       // Scanner s = new Scanner(System.in);
  
         int n = 6;
         char[] charArray = { 'a', 'b', 'c', 'd', 'e', 'f' };
@@ -27,44 +26,26 @@ public class App
             hn.left = null;
             hn.right = null;
  
-            // add functions adds
-            // the huffman node to the queue.
             pq.add(hn);
         }
         HuffmanNode root = null;
- 
-        // Here we will extract the two minimum value
-        // from the heap each time until
-        // its size reduces to 1, extract until
-        // all the nodes are extracted.
         while (pq.size() > 1) {
  
-            // first min extract.
+            
             HuffmanNode x = pq.peek();
             pq.poll();
  
-            // second min extarct.
             HuffmanNode y = pq.peek();
             pq.poll();
  
-            // new node f which is equal
             HuffmanNode f = new HuffmanNode();
  
-            // to the sum of the frequency of the two nodes
-            // assigning values to the f node.
             f.data = x.data + y.data;
             f.c = '-';
- 
-            // first extracted node as left child.
+
             f.left = x;
- 
-            // second extracted node as the right child.
             f.right = y;
- 
-            // marking the f node as the root node.
             root = f;
- 
-            // add this node to the priority-queue.
             pq.add(f);
         }
         printCode(root, "");
@@ -72,7 +53,7 @@ public class App
 
     public static void printCode(HuffmanNode root, String s){
         if (root.left == null && root.right == null && Character.isLetter(root.c)) {
-            // c is the character in the node
+            
             System.out.println(root.c + ":" + s);
             return;
         }
